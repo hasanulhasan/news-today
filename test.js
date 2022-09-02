c10206da7d52546db520ebb8b8546b58a9367f9a
Md Hasanul Karim <hasanlink71@yahoo.com>
2022-09-02 23:54:23 +0600

    menus fetched

--- test.js
+++ test.js
@@ -1,1 +1,20 @@
-console.log('taile amio ready')
+const loadData = async () =>{
+  const url = `https://openapi.programming-hero.com/api/news/categories`
+  const res = await fetch(url);
+  const data = await res.json();
+  displayCategories(data.data);
+}
+const displayCategories = (categories) =>{
+  const categoriesAll = categories.news_category;
+  categoriesAll.forEach(ctname => {
+    console.log(ctname.category_name);
+    const menus = document.getElementById('news-list');
+    const listMenu = document.createElement('li');
+    listMenu.innerHTML = `${ctname.category_name}
+    `
+    menus.appendChild(listMenu);
+  });
+  
+}
+
+loadData();
