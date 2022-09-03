21af78a937f1bfb94eb6868a96048109270ad3df
Md Hasanul Karim <hasanlink71@yahoo.com>
2022-09-03 14:18:31 +0600

    news is showing when clicked

--- test2.js
+++ test2.js
@@ -6,13 +6,30 @@ const loadCategory = (id) => {
 }
 const displayCategory = (news) =>{
   console.log(news);
+  
   news.forEach(element => {
+    const newsSection = document.getElementById('news-div');
+    const newsDiv = document.createElement('div');
+    newsDiv.innerHTML = `
+    <div class="row shadow-lg py-2">
+    <div class="col-4">
+      <img class="img-fluid p-1" src="juka.jpg" alt="">
+    </div>
+    <div class="border-1 bg-success p-3 col-8">
+      <h1 class="pb-3">${element.title}</h1>
+      <img src="juka.jpg" alt="" height="40px" width="40px">
+      <h5>${element.author.name}</h5>
+      <p class="pt-3">${element.details}</p>
+    </div>
+  </div>
+    `
+    newsSection.appendChild(newsDiv);
     console.log(element.category_id);
-    console.log(element.title);
+    // console.log(element.title);
     // console.log(element.details);
     // console.log(element._id);
     // console.log(element.rating.number);
-    console.log(element.author.name);
+    // console.log(element.author.name);
   });
 }
 
